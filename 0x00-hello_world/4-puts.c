3ac52ed63ab14151f6d3ccb383fbe63ae13a834d
Ujah Linus Sunday <ujahlinussunday@gmail.com>
2023-02-16 13:09:34 -0800

    Hello, puts.1

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A C program using puts function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
