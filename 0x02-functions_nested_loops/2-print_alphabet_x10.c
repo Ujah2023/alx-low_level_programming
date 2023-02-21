0c848f2f71b5baafc639d80bfa0811f7f6dee19d
Ujah Linus Sunday <ujahlinussunday@gmail.com>
2023-02-21 15:39:51 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet x10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int c;
+	char i = 0;
+
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
