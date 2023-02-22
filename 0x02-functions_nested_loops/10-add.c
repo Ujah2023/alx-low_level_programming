b2090dbb5ebb3d05ac1f559f1f759cce971d3e0e
Ujah Linus Sunday <ujahlinussunday@gmail.com>
2023-02-22 10:31:00 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: the addition of both
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
