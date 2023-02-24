9114b94b176c455c7004bc9817e116c716c0f535
Ujah Linus Sunday <ujahlinussunday@gmail.com>
2023-02-24 05:52:31 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line according to parameter
+ * @n: The number of lines to print
+ * Return: Empty
+ */
+
+void print_line(int n)
+{
+	int y;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (y = 0; y < n; y++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
