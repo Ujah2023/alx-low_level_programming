e0ba1a72db991cd89cf2e61831273e1d801f34aa
Ujah Linus Sunday <ujahlinussunday@gmail.com>
2023-02-24 15:18:43 -0800

    101-print_number.c update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- * print_number - prints an integer
- * @n: The integer to print
- * Return: Nothing
+ * print_number - prints an integer.
+ * @n: The integer to print.
+ * Return: Nothing!
  */
 
 void print_number(int n)
