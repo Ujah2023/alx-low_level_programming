3fa1bde65721738161b09811de70bc32087ba713
Ujah Linus Sunday <ujahlinussunday@gmail.com>
2023-03-06 11:21:18 -0800

    3-strspn.c.1 update

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,22 +9,22 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int in bytes = 0;
-	int index;
+	unsigned int n = 0;
+	int r;
 
 	while (*s)
 	{
-		for (index = 0; accept[index]; index++)
+		for (r = 0; accept[r]; r++)
 		{
-			if (*s == accept[index])
+			if (*s == accept[r])
 			{
-				bytes++;
+				n++;
 				break;
 			}
-			else if (accept[index + 1] == '\0')
-				return (bytes);
+			else if (accept[r + 1] == '\0')
+				return (n);
 		}
 		s++;
 	}
-	return (bytes);
+	return (n);
 }
